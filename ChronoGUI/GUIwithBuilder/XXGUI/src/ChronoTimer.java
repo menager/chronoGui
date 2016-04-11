bb55cc73bff5185806815770afd2fc59e8379d7b
Dave Faber <dcfaber@uwm.edu>
2016-04-10 21:22:33 -0500

    Updated PNG image to block out text, added rest of buttons

--- ChronoGUI/GUIwithBuilder/XXGUI/src/ChronoTimer.java
+++ ChronoGUI/GUIwithBuilder/XXGUI/src/ChronoTimer.java
@@ -24,6 +24,9 @@ import javax.swing.border.BevelBorder;
 import java.awt.Font;
 import javax.swing.JToggleButton;
 import javax.swing.JRadioButton;
+import javax.swing.JTextArea;
+import java.awt.SystemColor;
+import javax.swing.UIManager;
 
 public class ChronoTimer extends JFrame {
 
@@ -60,7 +63,7 @@ public class ChronoTimer extends JFrame {
 		JLabel lblNewLabel = new JLabel("New label");
 		lblNewLabel.setBounds(5, 5, 615, 510);
 		lblNewLabel.setOpaque(true);
-		lblNewLabel.setIcon(new ImageIcon("C:\\Users\\Morgan\\workspace\\XXGUI\\src\\chrono.jpg"));
+		lblNewLabel.setIcon(new ImageIcon("C:\\Users\\Dave\\git\\chronoGui\\ChronoGUI\\GUIwithBuilder\\XXGUI\\chrono.jpg"));
 		contentPane.add(lblNewLabel);
 		
 		JButton btn1 = new JButton("1");
@@ -189,5 +192,108 @@ public class ChronoTimer extends JFrame {
 		enable7.setBackground(Color.WHITE);
 		enable7.setBounds(362, 80, 30, 25);
 		contentPane.add(enable7);
+		
+		JButton btnPower = new JButton("Power");
+		btnPower.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		btnPower.setBounds(60, 29, 91, 30);
+		contentPane.add(btnPower);
+		
+		JButton btnFunction = new JButton("FUNCTION");
+		btnFunction.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		btnFunction.setBounds(48, 185, 107, 25);
+		contentPane.add(btnFunction);
+		
+		JButton btnSwap = new JButton("SWAP");
+		btnSwap.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		btnSwap.setBounds(62, 276, 89, 27);
+		contentPane.add(btnSwap);
+		
+		JButton btnPrinterPwr = new JButton("Printer Pwr");
+		btnPrinterPwr.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		btnPrinterPwr.setBounds(444, 21, 108, 25);
+		contentPane.add(btnPrinterPwr);
+		
+		JTextArea mainDisplay = new JTextArea();
+		mainDisplay.setText("");
+		mainDisplay.setBounds(243, 183, 149, 135);
+		contentPane.add(mainDisplay);
+		
+		JTextArea txtrPrinterarea = new JTextArea();
+		txtrPrinterarea.setText("");
+		txtrPrinterarea.setBounds(454, 60, 84, 80);
+		contentPane.add(txtrPrinterarea);
+		
+		JRadioButton chan1 = new JRadioButton("");
+		chan1.setBackground(Color.WHITE);
+		chan1.setBounds(94, 409, 28, 25);
+		contentPane.add(chan1);
+		
+		JRadioButton chan3 = new JRadioButton("");
+		chan3.setBackground(Color.WHITE);
+		chan3.setBounds(124, 409, 26, 25);
+		contentPane.add(chan3);
+		
+		JRadioButton chan5 = new JRadioButton("");
+		chan5.setBackground(Color.WHITE);
+		chan5.setBounds(154, 409, 24, 25);
+		contentPane.add(chan5);
+		
+		JRadioButton chan7 = new JRadioButton("");
+		chan7.setBackground(Color.WHITE);
+		chan7.setBounds(185, 409, 31, 25);
+		contentPane.add(chan7);
+		
+		JRadioButton chan2 = new JRadioButton("");
+		chan2.setBackground(Color.WHITE);
+		chan2.setBounds(91, 447, 24, 25);
+		contentPane.add(chan2);
+		
+		JRadioButton chan4 = new JRadioButton("");
+		chan4.setBackground(Color.WHITE);
+		chan4.setBounds(124, 447, 27, 25);
+		contentPane.add(chan4);
+		
+		JRadioButton chan6 = new JRadioButton("");
+		chan6.setBackground(Color.WHITE);
+		chan6.setBounds(154, 447, 26, 25);
+		contentPane.add(chan6);
+		
+		JRadioButton chan8 = new JRadioButton("");
+		chan8.setBackground(Color.WHITE);
+		chan8.setBounds(185, 447, 26, 25);
+		contentPane.add(chan8);
+		
+		JButton start1 = new JButton("");
+		start1.setBackground(UIManager.getColor("Button.background"));
+		start1.setBounds(270, 62, 22, 15);
+		contentPane.add(start1);
+		
+		JButton start3 = new JButton("");
+		start3.setBounds(304, 62, 20, 17);
+		contentPane.add(start3);
+		
+		JButton start5 = new JButton("");
+		start5.setBounds(330, 64, 23, 16);
+		contentPane.add(start5);
+		
+		JButton start7 = new JButton("");
+		start7.setBounds(360, 62, 23, 18);
+		contentPane.add(start7);
+		
+		JButton finish2 = new JButton("");
+		finish2.setBounds(273, 126, 18, 14);
+		contentPane.add(finish2);
+		
+		JButton finish4 = new JButton("");
+		finish4.setBounds(302, 125, 20, 18);
+		contentPane.add(finish4);
+		
+		JButton finish6 = new JButton("");
+		finish6.setBounds(330, 125, 25, 16);
+		contentPane.add(finish6);
+		
+		JButton finish8 = new JButton("");
+		finish8.setBounds(362, 125, 23, 18);
+		contentPane.add(finish8);
 	}
 }
